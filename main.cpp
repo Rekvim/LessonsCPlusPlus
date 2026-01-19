5b7544a0ce5115f05e9b69adfd67dac1079d9127
Rekvim <151747849+Rekvim@users.noreply.github.com>
2026-01-19 13:54:03 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,63 +1,19 @@
-#include <vector>
-#include <string>
 #include <iostream>
-#include <map>
-#include <numeric>
+#include <vector>
 
-struct User {
-    std::string name;
-    int age{};
-};
-
-int sum(const std::vector<int>& vector)
-{
-    return std::accumulate(vector.begin(), vector.end(), 0);
-}
-
-User get_user(bool create)
-{
-    if (create) {
-        return User{.name = "Alice", .age=30};
-    }
-
-    return User{.name = "Admin", .age = 999};
-}
-
-void print_name(const User& user)
-{
-    if (user.name == "") {                // ❌ лучше .empty()
-        std::cout << "Empty\n";
-    }
-}
-
-void process(std::vector<int>& data)
-{
-    for (auto it = data.begin(); it != data.end(); ++it) {
-        if (*it < 0) {
-            data.erase(it);
+void PrintWorryCount(const std::vector<bool>& is_nervous) {
+    // подсчитываем количество элементов в векторе is_nervous, равных true
+    int worry_count = 0;
+    for (bool status : is_nervous) {
+        if (status) {
+            ++worry_count;
         }
     }
+    std::cout << worry_count << '\n';
 }
 
-int main()
-{
-    std::vector<int> values = {1, 2, 3};
-
-    std::cout << sum(values) << '\n';
-
-    const User user = get_user(true);
-    print_name(user);
-
-    const int value = 42;
-    std::cout << value << '\n';
-
-    std::map<std::string, int> word_to_count;
-    word_to_count.emplace("one", 1);
-    word_to_count.emplace("two", 2);
-
-    for (const auto& [key, value] : word_to_count) { 
-        std::cout << key << '\n';
-    }
-
+int main() {
+    // не меняйте содержимое функции main
+    PrintWorryCount({true, true, false, true});
     return 0;
 }
