c23b29928ea87ac1ea2acc34f278260b7e264b5f
Rekvim <151747849+Rekvim@users.noreply.github.com>
2026-01-19 19:13:50 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,44 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <utility>
 
-void PrintWorryCount(const std::vector<bool>& is_nervous) {
-    // подсчитываем количество элементов в векторе is_nervous, равных true
-    int worry_count = 0;
-    for (bool status : is_nervous) {
-        if (status) {
-            ++worry_count;
-        }
+using std::literals::string_literals::operator""s;
+
+std::pair<bool, double> CalcMedian(std::vector<double> samples) {
+    if (samples.empty()) {
+        return {false, 0.0};
     }
-    std::cout << worry_count << '\n';
+
+    std::sort(samples.begin(), samples.end());
+
+    const std::size_t mid_index = samples.size() / 2;
+
+    if (samples.size() % 2 == 0) {
+        return  {true, (samples[mid_index - 1] + samples[mid_index]) / 2.0};
+    }
+
+    return {true, samples[mid_index]};
 }
 
 int main() {
-    // не меняйте содержимое функции main
-    PrintWorryCount({true, true, false, true});
-    return 0;
+    int size = 0;
+    
+    std::cin >> size;
+    // int x = "oops";
+    std::vector<double> samples;
+    for (int i = 0; i < size; ++i) {
+        double sample = 0.0;
+        std::cin >> sample;
+        samples.push_back(sample);
+    }
+
+    const auto [median_exists, median_value] = CalcMedian(samples);
+
+    if (median_exists) {
+        std::cout << median_value << '\n';
+    } else {
+        std::cout << "Empty vector\n"s;
+    }
+
 }
